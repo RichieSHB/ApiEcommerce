8e4905ba4f31b76d106bc44f781a672ee73b2980
Ricardo <richieshb@gmail.com>
2026-01-12 11:11:29 -0600

    Modelo + Migracion

---
+++ ApiEcommerce/Models/Product.cs
@@ -0,0 +1,37 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiEcommerce.Models;
+
+public class Product
+{
+    [Key]
+    public int ProductId { get; set; }
+
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+
+    [Range(0,double.MaxValue)]
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Price { get; set; }
+
+    public string ImgUrl { get; set; } = string.Empty;
+
+    [Required]
+    public string SKU { get; set; } = string.Empty;
+
+    [Range(0, int.MaxValue)]
+    public int Stock { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime? UpdatedAt { get; set; } = null;
+
+    // Foreign Key to Category
+    public int CategoryId { get; set; }
+
+    [ForeignKey("CategoryId")]
+    public required Category Category { get; set; }
+}
